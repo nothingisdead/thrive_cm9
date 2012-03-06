0b34dbbba3c8cd8d7fee927dd5357d32b812df0d
dcmarquardt <dcmarquardt@infernodev.com>
2012-03-06 08:36:52 -0600

    Fixed the build script to make the process of creating an installable zip file easier.

--- build.bat
+++ build.bat
@@ -1,14 +1,35 @@
 @setlocal
 @echo off
 
+rem Set up all of the java variables (may vary by machine)
 set JDK_HOME=C:\Java\jdk1.6.0_25
 set JAVA_EXE=%JDK_HOME%\bin\java.exe
 set JAR_EXE=%JDK_HOME%\bin\jar.exe
 
-set TODAY=%year%-%month%-%day%-%hour%-%minutes%
+rem DO NOT CHANGE ANYTHING BELOW THIS LINE
 
+rem Date & Time variables for the build number and file name
+echo %date%
+set year=%date:~-4%
+set month=%date:~7,2%
+set day=%date:~4,2%
+set hour=%time:~0,2%
+if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
+set min=%time:~3,2%
+if "%min:~0,1%" == " " set min=0%min:~1,1%
+set secs=%time:~6,2%
+if "%secs:~0,1%" == " " set secs=0%secs:~1,1%
+
+set BUILDSTAMP=%year%-%day%-%month%_%hour%%min%%secs%
+
+rem Create the unsigned zip file in the dir that we are currently in
 %JAR_EXE% cf %CD%\unsigned-update.zip -C %CD%\src\ .
 
-%JAVA_EXE% -classpath %CD%\build\testsign.jar testsign %CD%\unsigned-update.zip update-%TODAY%.zip
+rem Sign the build file
+%JAVA_EXE% -classpath %CD%\build\testsign.jar testsign %CD%\unsigned-update.zip update-%BUILDSTAMP%.zip
+
+rem Delete the unsigned zip file as we no longer need it
+del unsigned-update.zip
+
 
 
